f577c025aad8e3d3111a1b2c1fd6a49382775927
devon <devon@huazai.cloud>
2025-04-07 11:11:08 +0800

    CollectionUtils 优化

--- component-tool/src/main/java/cloud/huazai/tool/java/util/CollectionUtils.java
+++ component-tool/src/main/java/cloud/huazai/tool/java/util/CollectionUtils.java
@@ -84,6 +84,75 @@ public class CollectionUtils {
         return JsonUtils.toJsonString(collection);
     }
 
+    /**
+     * 交集
+     * @param coll1 集合1
+     * @param coll2 集合2
+     * @param <T> 对象类型
+     * @return 交集集合
+     *
+     */
+    public static <T> Collection<T> intersection(Collection<T> coll1, Collection<T> coll2) {
+        List<T> result = new ArrayList<>(coll1);
+        result.retainAll(coll2); // 保留两个集合都有的元素
+        return result;
+    }
+
+    /**
+     * 并集
+     * @param coll1 集合1
+     * @param coll2 集合2
+     * @param <T> 对象类型
+     * @return 并集集合
+     */
+    public static <T> Collection<T> union(Collection<T> coll1, Collection<T> coll2) {
+        Set<T> set = new HashSet<>(coll1);
+        set.addAll(coll2); // 添加所有元素到集合中（自动去重）
+        return new ArrayList<>(set);
+    }
+
+
+    /**
+     * 差集
+     * @param coll1
+     * @param coll2
+     * @param <T>
+     * @return
+     */
+    public static <T> Collection<T> difference(Collection<T> coll1, Collection<T> coll2) {
+        List<T> result = new ArrayList<>(coll1);
+        result.removeAll(coll2); // 移除 collection1 中存在于 collection2 的元素
+        return result;
+    }
+
+
+    /**
+     * 补集
+     * @param universalColl 全集
+     * @param coll 子集
+     * @param <T> 对象类型
+     * @return 补集集合
+     */
+    public static <T> Collection<T> complement(Collection<T> universalColl, Collection<T> coll) {
+        List<T> result = new ArrayList<>(universalColl);
+        result.removeAll(coll); // 移除子集中的元素
+        return result;
+    }
+
+    /**
+     * 对称差集
+     * @param collection1
+     * @param collection2
+     * @return
+     * @param <T>
+     */
+    public static <T> Collection<T> symmetricDifference(Collection<T> collection1, Collection<T> collection2) {
+        Collection<T> union = union(collection1, collection2); // 并集
+        Collection<T> intersection = intersection(collection1, collection2); // 交集
+        union.removeAll(intersection); // 移除交集部分
+        return union;
+    }
+
 
 
 }

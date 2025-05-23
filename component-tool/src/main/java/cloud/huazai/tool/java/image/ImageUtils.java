06dfdca7adbd3c6835bb95dcf71872bab0b5ac20
devon <devon@huazai.cloud>
2025-05-23 16:30:50 +0800

    优化

--- component-tool/src/main/java/cloud/huazai/tool/java/image/ImageUtils.java
+++ component-tool/src/main/java/cloud/huazai/tool/java/image/ImageUtils.java
@@ -22,47 +22,82 @@ public class ImageUtils {
 
     /**
      * compressImage
-     * @param inputStream inputStream
+     *
+     * @param inputStream  inputStream
      * @param outputStream outputStream
-     * @param quality quality
+     * @param quality      quality
      */
     public static void compressImage(InputStream inputStream, OutputStream outputStream, float quality) {
 
-        ImageWriter writer = null;
-        ImageOutputStream ios = null;
+        ImageOutputStream imageOutputStream = null;
         try {
             BufferedImage image = ImageIO.read(inputStream);
-
-            if (image.getColorModel().getPixelSize() != 24) {
-                BufferedImage rgbImage = new BufferedImage(
-                        image.getWidth(),
-                        image.getHeight(),
-                        BufferedImage.TYPE_INT_RGB
-                );
-                rgbImage.getGraphics().drawImage(image, 0, 0, null);
-                image = rgbImage;
-            }
-
-            Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("jpg");
-
-            if (!writers.hasNext()) {
-                throw new IllegalStateException("No writers found");
-            }
-            writer = writers.next();
-            ios = ImageIO.createImageOutputStream(outputStream);
-            writer.setOutput(ios);
-
-            ImageWriteParam param = writer.getDefaultWriteParam();
-            param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-            param.setCompressionQuality(quality);
-            writer.write(null, new IIOImage(image, null, null), param);
-
+            imageOutputStream = ImageIO.createImageOutputStream(outputStream);
+            compressImage(image,imageOutputStream, quality);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         } finally {
             // 关闭相关的流
-            IOUtils.safeClose(ios);
+            IOUtils.safeClose(imageOutputStream);
+        }
+    }
+
+
+    public static void compressImage(File inputFile, File outputFile, float quality) {
+
+        // 检查输入文件是否存在
+        if (!inputFile.exists()) {
+            throw new IllegalArgumentException("Input file does not exist.");
+        }
+
+        ImageOutputStream imageOutputStream = null;
+        try {
+            // 获取输入图像
+            BufferedImage image = ImageIO.read(inputFile);
+            imageOutputStream = ImageIO.createImageOutputStream(outputFile);
+            compressImage(image,imageOutputStream,quality);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            IOUtils.safeClose(imageOutputStream);
+        }
+    }
+
+
+
+    private static void compressImage(BufferedImage image, ImageOutputStream imageOutputStream, float quality)  {
+        if (image.getColorModel().getPixelSize() != 24) {
+            BufferedImage rgbImage = new BufferedImage(
+                    image.getWidth(),
+                    image.getHeight(),
+                    BufferedImage.TYPE_INT_RGB
+            );
+            rgbImage.getGraphics().drawImage(image, 0, 0, null);
+            image = rgbImage;
+        }
+        // 获取所有可用的图像写入器
+        Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("jpg");
+
+        if (!writers.hasNext()) {
+            throw new IllegalStateException("No writers found");
+        }
+
+        ImageWriter writer = writers.next();
+
+        writer.setOutput(imageOutputStream);
+
+        // 设置压缩参数
+        ImageWriteParam param = writer.getDefaultWriteParam();
+        param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+        param.setCompressionQuality(quality); // 压缩质量
+
+        // 写入图像
+        try {
+            writer.write(null, new IIOImage(image, null, null), param);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
             IOUtils.safeDispose(writer);
         }
     }
--- component-tool/src/test/java/cloud/huazai/tool/java/image/ImageUtilsTest.java
+++ component-tool/src/test/java/cloud/huazai/tool/java/image/ImageUtilsTest.java
@@ -2,22 +2,35 @@ package cloud.huazai.tool.java.image;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.OutputStream;
+import java.io.InputStream;
 import java.nio.file.Files;
+
 public class ImageUtilsTest {
 
     @Test
     void compressImage() throws Exception {
 
-        ByteArrayOutputStream outputStream = ImageUtils.compressImage(Files.newInputStream(new File("/Users/devon/Documents/壁纸.png").toPath()), 0.5f);
+        // ByteArrayOutputStream outputStream = ImageUtils.compressImage(Files.newInputStream(new File("/Users/devon/Documents/壁纸.png").toPath()), 0.5f);
+        //
+        // File file = new File("/Users/devon/Documents/壁纸5.png");
+        // OutputStream fileOutputStream = new FileOutputStream(file);
+        //
+        // outputStream.writeTo(fileOutputStream);
+        File file = new File("/Users/devon/Documents/壁纸.png");
+        String fileName = file.getName();
+        // 获取文件后缀
+        String suffix = "";
+        if (fileName.contains(".")) {
+            suffix = fileName.substring(fileName.lastIndexOf("."));
+        } else {
+            suffix = ".png";
+        }
+        InputStream inputStream = Files.newInputStream(new File("/Users/devon/Documents/壁纸.png").toPath());
+        FileOutputStream fileOutputStream = new FileOutputStream(File.createTempFile(String.valueOf(System.currentTimeMillis()), suffix));
+        ImageUtils.compressImage(inputStream, fileOutputStream, 0.5f);
 
-        File file = new File("/Users/devon/Documents/壁纸5.png");
-        OutputStream fileOutputStream = new FileOutputStream(file);
-
-        outputStream.writeTo(fileOutputStream);
 
     }
 }

01da804be4277b3e378b7f8f80390845de0259f6
devon <devon@huazai.cloud>
2026-03-03 09:11:37 +0800

    优化

--- component-tool/src/test/java/cloud/huazai/tool/java/image/ImageUtilsTest.java
+++ component-tool/src/test/java/cloud/huazai/tool/java/image/ImageUtilsTest.java
@@ -2,11 +2,6 @@ package cloud.huazai.tool.java.image;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.nio.file.Files;
-
 public class ImageUtilsTest {
 
     @Test
@@ -18,19 +13,6 @@ public class ImageUtilsTest {
         // OutputStream fileOutputStream = new FileOutputStream(file);
         //
         // outputStream.writeTo(fileOutputStream);
-        File file = new File("/Users/devon/Documents/壁纸.png");
-        String fileName = file.getName();
-        // 获取文件后缀
-        String suffix = "";
-        if (fileName.contains(".")) {
-            suffix = fileName.substring(fileName.lastIndexOf("."));
-        } else {
-            suffix = ".png";
-        }
-        InputStream inputStream = Files.newInputStream(new File("/Users/devon/Documents/壁纸.png").toPath());
-        FileOutputStream fileOutputStream = new FileOutputStream(File.createTempFile(String.valueOf(System.currentTimeMillis()), suffix));
-        ImageUtils.compressImage(inputStream, fileOutputStream, 0.5f);
-
 
     }
 }
